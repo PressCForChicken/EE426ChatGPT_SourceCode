e1a207b7b8882810bf71ef2c5f9cd3b1b8081e0f
PressCForChicken <rnoonan613@gmail.com>
2024-10-15 02:18:52 -0400

    Create ReferncesActivity.kt

---
+++ ReferncesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_references)  // Link to the layout you created
+    }
+}
